e916c204bf7f44fe3dd0c74e8f54398eeea13392
juj <serrgevv321@gmail.com>
2024-09-08 21:17:05 +0300

    finish project

--- components/Table.vue
+++ components/Table.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import { usePostsStore } from "~/store/tasksStore";
-
 const store = usePostsStore();
-const { fetchData } = store;
 const { data } = storeToRefs(store);
-onMounted(() => {
-  fetchData();
-});
 </script>
 <template>
-  <DataTable :value="data" stripedRows tableStyle="min-width: 50rem">
-    <Column field="userId" header="userId"></Column>
-    <Column field="id" sortable header="id"></Column>
-    <Column field="title" header="title"></Column>
-    <Column field="body" header="body"></Column>
-  </DataTable>
+  <div class="flex justify-center items-center w-full">
+    <DataTable :value="data" stripedRows tableStyle="min-width: 50rem">
+      <Column field="userId" header="userId"></Column>
+      <Column field="id" sortable header="id"></Column>
+      <Column field="title" header="title"></Column>
+      <Column field="body" header="body"></Column>
+    </DataTable>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { usePostsStore } from "~/store/tasksStore";
+
+const store = usePostsStore();
+const { isLoading } = storeToRefs(store);
+const { fetchData } = store;
+onMounted(() => {
+  fetchData();
+});
+</script>
 <template>
-  <div class="min-w-full">
+  <div
+    class="flex justify-center items-center w-full h-[80vh]"
+    v-if="isLoading"
+  >
+    <ProgressSpinner />
+  </div>
+  <div class="min-w-full" v-else>
     <Table />
     <div
       class="flex justify-between max-sm:flex-col max-sm:items-center max-sm:gap-y-4 mt-5"
